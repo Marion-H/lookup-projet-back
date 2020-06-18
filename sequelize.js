f3c3417853796fc9f89bddae87c6563948f75d68
Marion-H <doudou6500@gmail.com>
2020-06-18 11:14:27 +0200

    add DB_HOST in sequelize.js

--- sequelize.js
+++ sequelize.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 const Sequelize = require("sequelize");
 
-const { DB_NAME, DB_USER, DB_PASSWORD , NODE_ENV, DB_DIALECT, DB_TEST} = process.env;
+const { DB_NAME, DB_USER, DB_PASSWORD , NODE_ENV, DB_DIALECT, DB_TEST, DB_HOST} = process.env;
 
 module.exports = new Sequelize({
-    host: 'localhost',
+    host: DB_HOST,
     username: DB_USER,
     password: DB_PASSWORD,
     database: NODE_ENV !== "test" ? DB_NAME : DB_TEST,
