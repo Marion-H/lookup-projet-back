12e98a1869c0428048a11f5f4f8ed2f62cb35138
Marion-H <doudou6500@gmail.com>
2020-06-18 10:55:45 +0200

    add a DB_TEST in github main and sequelize.js

--- sequelize.js
+++ sequelize.js
@@ -1,13 +1,13 @@
 require('dotenv').config();
 const Sequelize = require("sequelize");
 
-const { DB_NAME, DB_USER, DB_PASSWORD , NODE_ENV, DB_DIALECT} = process.env;
+const { DB_NAME, DB_USER, DB_PASSWORD , NODE_ENV, DB_DIALECT, DB_TEST} = process.env;
 
 module.exports = new Sequelize({
     host: 'localhost',
     username: DB_USER,
     password: DB_PASSWORD,
-    database: NODE_ENV !== "test" ? DB_NAME : "quick_api_test",
+    database: NODE_ENV !== "test" ? DB_NAME : DB_TEST,
     dialect: DB_DIALECT, 
     logging: false,
 });
