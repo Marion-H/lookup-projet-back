550fdc0dea3ebc7fac6b53ed75153da60cfcc5f2
Marion-H <doudou6500@gmail.com>
2020-06-23 16:52:20 +0200

    create a new route get on product info and a new test for that

--- routes/product_info.route.js
+++ routes/product_info.route.js
@@ -8,12 +8,22 @@ const { uuidv4RegExp } = require("../middlewares/regexCheck");
 const ProductInfo = require("../model/product_info.model");
 
 product_info.get("/", async (req, res) => {
-    const products = await ProductInfo.findAll();
-    try {
-      res.status(200).json(products);
-    } catch (err) {
-      res.status(400).json(err);
-    }
-  });
+  const products = await ProductInfo.findAll();
+  try {
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+product_info.get("/:uuid", regExpIntegrityCheck(uuidv4RegExp), async (req, res) => {
+  const uuid = req.params.uuid;
+  try {
+    const products = await ProductInfo.findOne({ where: { uuid } });
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
 module.exports = product_info;
