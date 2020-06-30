b834aa2c598c3f751b9cd7c7b9b99ec6935ec9db
Neo-Ryo <marco.sch4064@gmail.com>
2020-06-30 17:44:20 +0200

    making client route and test

---
+++ model/client.model.js
@@ -0,0 +1,45 @@
+const Sequelize = require("sequelize");
+const sequelizeInstance = require("../sequelize");
+
+const Client = sequelizeInstance.define("Client", {
+  uuid: {
+    type: Sequelize.UUID,
+    allowNull: false,
+    primaryKey: true,
+    defaultValue: Sequelize.UUIDV4,
+  },
+  companyName: {
+    type: Sequelize.STRING(100),
+    allowNull: false,
+  },
+  streetNumber: {
+    type: Sequelize.INTEGER,
+    allowNull: true,
+  },
+  streetName: {
+    type: Sequelize.STRING(100),
+    allowNull: false,
+  },
+  city: {
+    type: Sequelize.STRING(100),
+    allowNull: false,
+  },
+  postalCode: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+  },
+  email: {
+    type: Sequelize.STRING(255),
+    allowNull: false,
+  },
+  phone: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+  },
+  siret: {
+    type: Sequelize.STRING(14),
+    allowNull: false,
+  },
+});
+
+module.exports = Client;
