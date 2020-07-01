b793b07435f007c8224370bd09417ac31fdfe577
Marion-H <doudou6500@gmail.com>
2020-07-01 10:01:51 +0200

    add a new route get one and a test for route press

--- routes/press.route.js
+++ routes/press.route.js
@@ -7,14 +7,25 @@ const { uuidv4RegExp } = require("../middlewares/regexCheck");
 
 const Press = require("../model/press.model");
 
-
 press.get("/", async (req, res) => {
-    const pressR = await Press.findAll();
-    try {
-      res.status(200).json(pressR);
-    } catch (err) {
-      res.status(400).json(err);
-    }
-  });
+  const pressR = await Press.findAll();
+  try {
+    res.status(200).json(pressR);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
+press.get("/:uuid", regExpIntegrityCheck(uuidv4RegExp), async (req, res) => {
+  const uuid = req.params.uuid;
+  try {
+    const pressR = await Press.findByPk(uuid);
+    res.status(200).json(pressR);
+  } catch (err) {
+    res.status(422).json({
+      status: "error",
+      message: "invalid request",
+    });
+  }
+});
 module.exports = press;
--- test/press.test.js
+++ test/press.test.js
@@ -11,41 +11,54 @@ const sequelize = require("../sequelize");
 chai.use(chaiHttp);
 
 const pressKey = [
-    "uuid",
-    "title",
-    "description",
-    "picture",
-    "createdAt",
-    "updatedAt",
-  ];
-  
-  let press;
+  "uuid",
+  "title",
+  "description",
+  "picture",
+  "createdAt",
+  "updatedAt",
+];
 
-  describe("PRESS", () => {
-    before(async () => {
-      await sequelize.sync({force : true });
-  
-  
-      press = await Press.create({
-        title: "test",
-        description: "Loreum ipsum",
-        picture: "https://www.test.fr/test.jpg",
-      });
+let press;
+
+describe("PRESS", () => {
+  before(async () => {
+    await sequelize.sync({ force: true });
+
+    press = await Press.create({
+      title: "test",
+      description: "Loreum ipsum",
+      picture: "https://www.test.fr/test.jpg",
     });
+  });
 
-    describe("get all press relation", () => {
-        it("should return an array of press relation", async () => {
-          try {
-            const res = await chai.request(server).get("/press");
-            res.should.have.status(200);
-            res.body.should.be.a("array");
-            res.body[0].should.have.keys(pressKey);
-            res.body.length.should.be.eql(1);
-          } catch (err) {
-            throw err;
-          }
-        });
-      });
-    
+  describe("get all press relation", () => {
+    it("should return an array of press relation", async () => {
+      try {
+        const res = await chai.request(server).get("/press");
+        res.should.have.status(200);
+        res.body.should.be.a("array");
+        res.body[0].should.have.keys(pressKey);
+        res.body.length.should.be.eql(1);
+      } catch (err) {
+        throw err;
+      }
+    });
+  });
+
+  describe("get a press relation", () => {
+    it("should return an unique press relation", async () => {
+      try {
+        const res = await chai
+          .request(server)
+          .get(`/press/${press.uuid}`);
+        res.should.have.status(200);
+        res.body.should.be.a("object");
+      } catch (err) {
+        throw err;
+      }
+    });
+  });
+
 
 });
