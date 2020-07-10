65a8b263e690e9716b395147ce00004717edf6d4
Karnak19 <basile64.v@gmail.com>
2020-07-10 10:40:16 +0200

    changing way to allow API tools from only development env to everything except production env

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const whitelist = process.env.CLIENT_URLS.split(", ");
 app.use(
   cors({
     origin: function (origin, callback) {
-      if (whitelist.indexOf(origin) !== -1 || (env === "development" && !origin)) {
+      if (whitelist.indexOf(origin) !== -1 || (env !== "production" && !origin)) {
         callback(null, true);
       } else {
         callback(new Error("Not allowed by CORS"));
