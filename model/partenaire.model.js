a8c568168d212aad8255b422fa8cda2a7e93757e
Neo-Ryo <marco.sch4064@gmail.com>
2020-07-16 14:34:43 +0200

    updated partner model key title to link

--- model/partenaire.model.js
+++ model/partenaire.model.js
@@ -1,25 +1,25 @@
 const Sequelize = require("sequelize");
 const sequelizeInstance = require("../sequelize");
 
-const Partenaire = sequelizeInstance.define("Partenaire",{
-    uuid: {
-        type: Sequelize.UUID,
-        allowNull: false,
-        primaryKey: true,
-        defaultValue: Sequelize.UUIDV4,
-    },
-    title: {
-        type: Sequelize.STRING(50),
-        allowNull: true,
-    },
-    description: {
-        type: Sequelize.STRING(50),
-        allowNull: false,
-    },
-    logo: {
-        type: Sequelize.STRING(250),
-        allowNull: false,
-    }
-})
+const Partenaire = sequelizeInstance.define("Partenaire", {
+  uuid: {
+    type: Sequelize.UUID,
+    allowNull: false,
+    primaryKey: true,
+    defaultValue: Sequelize.UUIDV4,
+  },
+  link: {
+    type: Sequelize.STRING,
+    allowNull: true,
+  },
+  description: {
+    type: Sequelize.STRING(50),
+    allowNull: false,
+  },
+  logo: {
+    type: Sequelize.STRING(250),
+    allowNull: false,
+  },
+});
 
-module.exports= Partenaire;
+module.exports = Partenaire;
