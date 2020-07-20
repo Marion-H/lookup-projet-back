4e4cd44e32aaf300e212341db5858a7260f8f03f
Neo-Ryo <marco.sch4064@gmail.com>
2020-07-20 11:40:20 +0200

    model product modified

--- model/press.model.js
+++ model/press.model.js
@@ -1,25 +1,25 @@
 const Sequelize = require("sequelize");
 const sequelizeInstance = require("../sequelize");
 
-const Press = sequelizeInstance.define("Press",{
-    uuid: {
-        type: Sequelize.UUID,
-        allowNull: false,
-        primaryKey: true,
-        defaultValue: Sequelize.UUIDV4,
-    },
-    title: {
-        type: Sequelize.STRING(50),
-        allowNull: true,
-    },
-    description: {
-        type: Sequelize.STRING(50),
-        allowNull: false,
-    },
-    picture: {
-        type: Sequelize.STRING(250),
-        allowNull: false,
-    }
-})
+const Press = sequelizeInstance.define("Press", {
+  uuid: {
+    type: Sequelize.UUID,
+    allowNull: false,
+    primaryKey: true,
+    defaultValue: Sequelize.UUIDV4,
+  },
+  title: {
+    type: Sequelize.STRING,
+    allowNull: true,
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  picture: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
 
-module.exports=Press;
+module.exports = Press;
